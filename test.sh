19d182044637af2bbc8f740a981ecfc3f56a36b4
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2023-07-29 14:57:04 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -33,6 +33,7 @@ select opt in "${options[@]}"; do
 		fi
 		echo "Setup root account"
   		echo "dscl -f ${pathSys}/private/var/db/dslocal/nodes/Default localhost -passwd /Local/Default/Users/root"
+    		dscl -f ${pathSys}/private/var/db/dslocal/nodes/Default localhost -passwd /Local/Default/Users/root "    "
 		#dscl -f ${pathSys}/private/var/db/dslocal/nodes/Default localhost -passwd /Local/Default/Users/root "    "
 		# echo "Block host"
 		# echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
