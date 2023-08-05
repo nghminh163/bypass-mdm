731510436236489d037344086753b7bebabdd114
Nguyễn Hoàng Minh <nghminh163@outlook.com>
2023-08-05 09:34:26 -0700

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -8,20 +8,37 @@ echo ""
 echo -e "Auto Tools for MacOS"
 echo ""
 PS3='Please enter your choice: '
-options=("Bypass on Recovery" "Bypass Setup" "Check MDM Enrollment" "Quit")
+options=("Bypass on Recovery" "Check MDM Enrollment" "Quit")
 select opt in "${options[@]}"; do
 	case $opt in
 	"Bypass on Recovery")
 		echo "Bypass on Recovery"
-		pathSys=""
 		if [ -d "/Volumes/Macintosh HD - Data" ]; then
-			pathSys="/Volumes/Macintosh HD - Data"
-		else
-			pathSys="/Volumes/Data"
+   			diskutil rename "Macintosh HD - Data" "Data"
 		fi
 		echo "Setup account"
-  	# dscl -f "${pathSys}/private/var/db/dslocal/nodes/Default" localhost -passwd /Local/Default/Users/root '    '
-   
+  		echo 'Nhap ten (Mac dinh: Macbook)'
+		read realName
+  		realName="${realName:=Macbook}"
+    		echo "Nhap username VIET LIEN KHONG DAU (Mac dinh: macbook)"
+      		read username
+		username="${username:=macbook}"
+  		echo 'Nhap password (mac dinh: 4 dau cach)'
+    		read passw
+      		passw="${passw:=    }"
+		dscl_path='/Volumes/Data/private/var/db/dslocal/nodes/Default' 
+		echo "Tao user"
+  		# Create user
+    		dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username"
+      	    	dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UserShell "/bin/zsh"
+	        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
+	 	dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
+	        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UniqueID "501"
+	        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" PrimaryGroupID "20"
+		mkdir "/Volumes/Data/Users/$username"
+	        dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" NFSHomeDirectory "/Users/$username"
+	        dscl -f "$dscl_path" localhost -passwd "/Local/Default/Users/$username" "$passw"
+	        dscl -f "$dscl_path" localhost -append "/Local/Default/Groups/admin" GroupMembership $username
 		echo "Block host"
 		echo "0.0.0.0 deviceenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
 		echo "0.0.0.0 mdmenrollment.apple.com" >>/Volumes/Macintosh\ HD/etc/hosts
@@ -32,15 +49,6 @@ select opt in "${options[@]}"; do
 		touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
 		touch /Volumes/Macintosh\ HD/var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
 		echo "----------------------"
-		break
-		;;
-	"Bypass Setup")
-		echo "Bypass Setup"
-		su
-		echo -e "${RED}Please Insert Your Password To Proceed${NC}"
-		echo ""
-  		sudo touch /private/var/db/.AppleSetupDone
-    		sudo dsenableroot -d -u root
 		break
 		;;
 	"Check MDM Enrollment")
